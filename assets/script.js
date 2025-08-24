815bbecbba44c4b252c96a4b8262bade81463cb1
Qipeng Qian <129544511+qqian99@users.noreply.github.com>
2025-08-24 12:11:23 -0700

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 function toggleNav(){
   const sb = document.querySelector('.sidebar');
   if(!sb) return;
-  if (sb.style.display === 'none'){ sb.style.display = 'block'; }
+  if (getComputedStyle(sb).display === 'none'){ sb.style.display = 'block'; }
   else { sb.style.display = 'none'; }
 }
